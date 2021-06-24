dddef57691b5ec014c984c4b1da2e6d169aafc77
Stefan Neidig <stefan@rpdev.net>
2021-06-24 10:55:23 +0200

    Add tools to tech radar

--- docs/data_architecture.js
+++ docs/data_architecture.js
@@ -1,40 +1,55 @@
 var architectureEntries = [
     {
-    ring: ADOPT,
-    label: "MVVM",
-    active: false,
-    moved: 0
-},
-{
-    ring: ADOPT,
-    label: "Coordinator",
-    active: false,
-    moved: 0
-},
-{
-    ring: ADOPT,
-    label: "MVP",
-    active: false,
-    moved: 0
-},
-{
-    ring: TRIAL,
-    label: "MVI",
-    active: false,
-    moved: 0
-},
-{
-    ring: ASSESS,
-    label: "MMVM + Redux",
-    active: false,
-    moved: 0
-},
-{
-    ring: HOLD,
-    label: "MVC",
-    active: false,
-    moved: 0
-}
+        ring: ASSESS,
+        label: "MS Power Apps",
+        active: false,
+        moved: 1
+    }, {
+        ring: ASSESS,
+        label: "Wappler",
+        active: false,
+        moved: 1
+    }, {
+        ring: ASSESS,
+        label: "App Smith",
+        active: false,
+        moved: 1
+    }, {
+        ring: ASSESS,
+        label: "Lobe.ai",
+        active: false,
+        moved: 1
+    }, {
+        ring: ASSESS,
+        label: "Ragic",
+        active: false,
+        moved: 1
+    }, {
+        ring: ASSESS,
+        label: "Budibase",
+        active: false,
+        moved: 1
+    }, {
+        ring: HOLD,
+        label: "Boundless",
+        active: false,
+        moved: 1
+    }, {
+        ring: ASSESS,
+        label: "Xano",
+        active: false,
+        moved: 1
+    }, {
+        ring: TRIAL,
+        label: "Peltarion",
+        active: false,
+        moved: 1
+    }, {
+        ring: TRIAL,
+        label: "Bubble.io",
+        active: false,
+        moved: 1
+    },
 ]
     .map(entry => {
         entry.quadrant = ARCHITECTURE;
